e9a60c6c5f138004ba9885b3dd8215b4b42c4a33
Jhanpiere Montes <jhanpieremontes@gmail.com>
2022-10-17 16:06:14 -0500

    Se distribuyo los componentes de Main y Card

--- src/components/Molecules/Card.jsx
+++ src/components/Molecules/Card.jsx
@@ -2,14 +2,47 @@
 function Card () {
   return (
     <article id='open-overlay-button' className='card'>
-      <img src='../static/images/quiz2_web.png' alt='Quiz 2'/>
-      <div class='card-text'>
-        <div>
-          <h3>Quiz 2 Web</h3>
-          <p>Conceptos propios de: Computer Science, Arquitectura de computadoras, Sistema Operativos, Redes y Web</p>
-        </div>
-        <button id='open-quiz-tooltip' class='button'>Ver la resolución del quiz</button>
-      </div>
+      <div class="card-quiz">
+					<div id="close-quiz-tooltip" class="cross">
+						<img src="./assets/cross.svg" alt="Cerrar tooltip">
+					</div>
+					<div class="card-quiz-text">
+						<section class="quiz-text-container">
+							<article>
+								<div class="quiz-theme-container">
+									<a class="enlace" href="http://137.184.108.27/~jhanpiere_montes/" target="_blank">
+										<h3 class="quiz-theme-title">Computer Science</h3>
+										<img src="./assets/share.svg" alt="Link de la clase">
+									</a>
+								</div>
+								<p>La Ciencia de la Computación  estudia los fundamentos de la información y la computación, puedo abarcar desde los teórico hasta los práctico respecto a estructuración de datos, programación, sistemas y/o estructuras computacionales.</p>
+							</article>
+							<article>
+								<h3>Arquitectura de Computadores</h3>
+								<p>Una computadora esta formada por diversos componentes los cuales hacen posible su funcionamiento:</p>
+								<ul>
+									<li>El CPU procesa cada intrucción y orden que se les da al dispositivo con la mayor efectividad posible.</li>
+									<li>El GPU es el encargado de realizar el renderizado de imagenes, videos, GIF, etc.</li>
+									<li>La Memoria RAM almancena una parte de la aplicación que ejecutamos, la suficiente para que garantize una experiencia cómoda al usuario.</li>
+									<li>Los SSD es un componente que almancenan cada programa u otros elementos de la PC, son formados por microchips conectadas entre sí.</li>
+									<li>Los periféricos son piezas que permiten conectar a la PC con las personas como teclados,monitores, mouse, etc.</li>
+								</ul>
+							</article>
+							<article>
+								<h3>Sistemas Operativos</h3>
+								<p>Los sistemas operativos son el soporte que facilita el uso de programas y cadenas de procesos mediante funciones, tienen 3 componentes importante los cuales son el sistema de archivos, traductor de comandos y nucleos.Existen diferentes sistemas operativos como Windows, MacOs, Ubuntu, Linux, Android, Ios, etc.</p>
+							</article>
+							<article>
+								<h3>Redes</h3>
+								<p>Las redes permiten conectar computadoras independientes, de esa forma nos simplifica el intercambio de datos y recursos.</p>
+							</article>
+							<article>
+								<h3>Web</h3>
+								<p>Es un sistema encargado de gestionar la información para una proxima trasmisión de datos(información, imagenes, video, audio, etc) al usuario a través de internet, además su utilización es muy común al momento de crear páginas web en lenguaje HTML.</p>
+							</article>
+						</section>
+					</div>
+				</div>
     </article>
   );
 }
--- src/components/Organisms/Main.jsx
+++ src/components/Organisms/Main.jsx
@@ -2,9 +2,109 @@ import Card from '../Molecules/Card';
 
 function Main () {
     return (
-        <main>
-            <Card />
-        </main>
+        <main class="main wrapper">
+		<div id="quiz-tooltip" class="quiz-tooltip">
+			<div class="quiz-tooltip-container">
+                <Card />
+			</div>
+		</div>
+		<section class="banner">
+			<img class="banner-image" src="./assets/avatar.png" alt="Profile">
+			<h1 class="h1"><span class="text-alt">Jhanpiere</span> <span class="text-alt">Montes</span></h1>
+			<p class="description">Estudiante de Ciencias de la computación</p>
+		</section>
+		<section id="homeworks">
+			<h2 class="h2">Tareas</h2>
+			<div class="card-container">
+				<div class="circle left"></div>
+				<!--<div class="circle right"></div>-->
+				<article id="open-overlay-button" class="card">
+					<img src="./assets/quiz2_web.png" alt="Quiz 2">
+					<div class="card-text">
+						<div>
+							<h3>Quiz 2 Web</h3>
+							<p>Conceptos propios de:</br>Computer Science, Arquitectura de computadoras, Sistema Operativos, Redes y Web</p>
+						</div>
+						<button id="open-quiz-tooltip" class="button">Ver la resolución del quiz</button>
+					</div>
+				</article>
+			</div>
+		</section>
+		<section class="projects" id="projects">
+			<h2 class="h2">Proyectos</h2>
+		</section>
+		<section class="about" id="about">
+			<h2 class="h2">Sobre mí</h2>
+			<div class="conter">
+				<article class="about-container">
+					<img src="./assets/desktop.jpg" alt="">
+					<div class="about-text">
+						<p>Soy de Lima, Peru. 🦙</p>
+						<p>Me encuentro cursando el primer ciclo de la carrera de Ciencias de la Computación en UTEC. 👨‍💻</p>
+						<p>Me gusta el trabajo en equipo, el aprendizaje constante y rodearme de personas apasiandas por lo que hacen.</p>
+					</div>
+				</article>
+				<article class="experience">
+					<div class="experience-container">
+						<div>
+							<h3>Educación</h3>
+							<p>Bachiller en Ciencias de la computación</p>
+							<p>Universidad de Ingeniería y Tecnología</p>
+							<p>Ago 2022 - Actualidad</p>
+						</div>
+						<div>
+							<h3>Idiomas</h3>
+							<p>Español (nativo)</p>
+							<p>Inglés (A2)</p>
+						</div>
+					</div>
+					<div>
+						<h3>Experiencia</h3>
+						<p>UX Designer</p>
+						<p>AGROS</p>
+						<p>Enero 2022 - Ago 2022</p>
+						<p>Investigación, implementación y creación de diseños en herramientas del Suite de la empresa</p>
+					</div>
+				</article>
+				<article class="skills-container">
+					<div>
+						<h3>Habilidades</h3>
+						<ul class="tools">
+							<li class="icon">
+								<img src="./assets/html.webp" alt="">
+							</li>
+							<li class="icon">
+								<img src="./assets/css.webp" alt="">
+							</li>
+							<li class="icon">
+								<img src="./assets/python.webp" alt="">
+							</li>
+							<li class="icon">
+								<img src="./assets/sass.webp" alt="">
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h3>Herramientas</h3>
+						<ul class="tools">
+							<li class="icon">
+								<img src="./assets/git.webp" alt="Git">
+							</li>
+							<li class="icon">
+								<img src="./assets/figma.webp" alt="Figma">
+							</li>
+							<li class="icon">
+								<img src="./assets/notion.webp" alt="Notion">
+							</li>
+							<li class="icon">
+								<img src="./assets/miro.webp" alt="Miro">
+							</li>
+						</ul>
+					</div>
+				</article>
+			</div>
+		</section>
+	</main>
     );
 }
 
